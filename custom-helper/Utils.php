a2182fef4b3139cd22665d0652ad4ac28a21538a
thienth32 <thienth32@gmail.com>
2017-05-28 18:51:45 +0700

    update recursive category

--- custom-helper/Utils.php
+++ custom-helper/Utils.php
@@ -5,20 +5,25 @@ if(!function_exists('prettyUrl')){
 	}
 }
 
-function get_options($array, $parent=0, $indent="", $forget = null) {
-    $return = array();
-    for ($i=0; $i < count($array); $i++) { 
-    	$val = $array[$i];
+if(!function_exists('get_options')){
 
-      	if($val->parent_id == $parent) {
-      		if($val->id == $forget){
-	    		continue;
-	    	}
-        	$return["x".$val->id] = $indent.$val->name;
-        	$return = array_merge($return, get_options($array, $val->id, $indent."--"));
-      	}
-    }
-    return $return;
+  function get_options($array, $parent=0, $indent="", $forget = null) {
+      
+      // Return variable
+      $return = array();
+      for ($i=0; $i < count($array); $i++) {
+          $val = $array[$i];
+
+        	if($val->parent_id == $parent && $val->id != $forget) {
+          	$return["x".$val->id] = $indent.$val->name;
+          	$return = array_merge($return, get_options($array, $val->id, $indent."--", $forget));
+          }
+      }
+
+      return $return;
+  }
 }
+
+
 
  ?>
